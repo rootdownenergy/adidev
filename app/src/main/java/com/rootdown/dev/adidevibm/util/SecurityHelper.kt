3a5bed681b1274aa295b69b29171647b1ce8ab6d
rootdownenergy <codedforweb@gmail.com>
2022-03-08 18:30:30 -0700

    up function ini unit jetsec

---
+++ app/src/main/java/com/rootdown/dev/adidevibm/util/SecurityHelper.kt
@@ -0,0 +1,55 @@
+package com.rootdown.dev.adidevibm.util
+
+import android.content.Context
+import android.os.Environment
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKeys
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.concurrent.ExecutorService
+import javax.inject.Inject
+import kotlin.properties.Delegates
+
+
+class SecurityHelper(appContext: Context) {
+
+    //livedata reactive stream
+    val streamGen: LiveDataReactiveStreams? = null
+    private var xByteFlag by Delegates.notNull<Int>()
+    private lateinit var outputDirectory: File
+    private lateinit var xExecutor: ExecutorService
+
+    val path = Environment.DIRECTORY_DOCUMENTS
+
+    init {
+        xStream()
+        outputDirectory = outputDirectory.absoluteFile
+    }
+    // Although you can define your own key generation parameter specification, it's
+    // recommended that you use the value specified here.
+    val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+    val mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+    val fileToRead = "my_sensitive_data.txt"
+    val encryptedFile = EncryptedFile.Builder(
+        File(outputDirectory, fileToRead),
+        appContext,
+        mainKeyAlias,
+        EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+    ).build()
+
+    val inputStream = encryptedFile.openFileInput()
+    val byteArrayOutputStream = ByteArrayOutputStream()
+
+    private fun xStream(){
+        var nextByte: Int = inputStream.read()
+        while (nextByte != -1)
+        {
+            byteArrayOutputStream.write(nextByte)
+            nextByte = inputStream.read()
+        }
+    }
+    val plaintext: ByteArray = byteArrayOutputStream.toByteArray()
+}
